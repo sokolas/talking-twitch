5a65df10eb5ecfe5fd05b98c03d7dc6cec3cc4c2
Sokolas <sunrise.beast@gmail.com>
2021-07-30 00:05:35 +0300

    all

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,67 @@
 import { createStore } from 'vuex'
+import tmi from 'tmi.js'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		voices: [],
+		client: null,
+		username: '',
+		password: '',
+		messages: [],
+		error: null
+	},
+	mutations: {
+		setClient(state, client) {
+			state.client = client;
+		},
+		onMessage(state, message) {
+			state.messages.push(message);
+			if (state.messages.length > 5) {
+				state.messages.shift();
+			}
+			// state.messages = state.messages.slice(-10);
+		},
+		setError(state, error) {
+			state.error = error;
+		}
+	},
+	actions: {
+		connect(context, username) {
+			if (context.state.client) {
+				context.state.client.disconnect();
+			}
+			const client = new tmi.Client({
+				options: { debug: true, messagesLogLevel: "info" },
+				connection: {
+					reconnect: true,
+					secure: true
+				},
+				channels: [ username ]
+			});
+			client.on('message', (channel, tags, message, self) => {
+				if (self) return;
+				context.commit('onMessage', {name: tags['display-name'], text: message});
+			});
+			client.on('connecting', () => {
+				context.commit('onMessage', {name: '###', text: 'connecting...'});
+			});
+			client.on('connected', () => {
+				context.commit('onMessage', {name: '###', text: 'connected'});
+			});
+			client.on('disconnected', (reason) => {
+				context.commit('onMessage', {name: '###', text: 'disconnected: ' + reason});
+			})
+			context.commit('setClient', client);
+			client
+				.connect()
+				.then()
+				.catch(e => {
+					context.commit('setClient', null);
+					context.commit('setError', e);
+				});
+			
+		}
+	},
+	modules: {
+	}
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Please go <a href="https://twitchapps.com/tmi/" target="blank">here</a>, generate and copy the token -->
+    <div>
+      <input type="text" placeholder="channel" id="username" v-model="name"/>
+      <button v-on:click="connect">Connect</button>
+      <input type="checkbox" v-model="saveToStorage" id="savetostore"/> <label for="savetostore">save voice for users</label>
+    </div>
+    <hr>
+    <div v-for="message in messages" :key=message>
+      <span><b>{{ message.name }} :</b> {{ message.text }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, computed, watch } from 'vue';
+import { useStore } from 'vuex';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const name = ref("");
+    const store = useStore();
+    function connect() {
+      console.log(name.value);
+      store.dispatch('connect', name.value);
+    }
+    const settings = {};
+    const saveToStorage = ref(false);
+
+    function save(user, voicesettings) {
+      if (saveToStorage.value) {
+        window.localStorage.setItem(user, JSON.stringify(voicesettings));
+      } else {
+        settings[user] = voicesettings;
+      }
+    }
+
+    function load(user) {
+      if (saveToStorage.value) {
+        const item = window.localStorage.getItem(user);
+        if (item) {
+          return JSON.parse(item);
+        } else {
+          return null;
+        }
+      } else {
+        return settings[user];
+      }
+    }
+    
+    watch(store.state.messages, (msgs) => {
+      const synth = window.speechSynthesis;
+      const voices = synth.getVoices();
+      if (voices.length == 0) return;
+      const lastMessage = msgs[msgs.length - 1];
+      const name = lastMessage.name;
+      const text = lastMessage.text;
+      console.log(name);
+      console.log(text);
+      if (name !== '###') {
+        let voiceSettings = load(name);
+        if (!voiceSettings) {
+          const i = Math.floor(Math.random() * voices.length);
+          const voice = voices[i];
+          const pitch = 2 * Math.random();
+          const rate = 5 * Math.random();
+          voiceSettings = {name: voice.name, pitch: pitch, rate: rate, lang: voice.lang};
+          save(name, voiceSettings);
+        }
+        console.log(JSON.stringify(voiceSettings));
+        const voice = voices.find(v => v.name === voiceSettings.name);
+        if (voice) {
+          const u = new SpeechSynthesisUtterance(text);
+          u.lang = voiceSettings.lang;
+          u.pitch = voiceSettings.pitch;
+          // u.rate = voiceSettings.rate;
+          synth.speak(u);
+        }
+      }
+    });
+    const messages = computed(() => store.state.messages);
+
+    return {
+      name, connect, messages, saveToStorage
+    }
+  },
+
+  name: 'Home'
 }
 </script>
