34d495550cc076af552a8eabfc44eb1f7c0289e3
Sokolas <sunrise.beast@gmail.com>
2021-08-02 23:40:12 +0300

    sound commands

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,20 +11,43 @@
       </form>
     </div>
     <hr>
+    <form v-on:submit.prevent="onSubmit">
+      <input type="file" v-on:change="fileChanged" id="file"/> <label for="file">Save sound</label>
+    </form>
+    <div v-for="(file, index) in soundFiles" :key="index">
+      <audio :src="file.url" :id="file.uuid" :ref="el => audios[file.uuid] = el" />{{ file.name }}&nbsp;
+        <button :id="file.uuid" v-on:click="playSound">&gt;</button>
+        <button :id="file.uuid" v-on:click="deleteSound">X</button>
+    </div>
+    <hr>
+    <div>
+      <form v-on:submit.prevent="addCommand">
+        <input type="text" placeholder="!command" id="command" v-model="command"/>
+        <input type="submit" value="Set" /><br>
+      </form>
+      <div v-for="(value, index) in soundCommands" :key="index">
+        {{value.name}}
+        <select v-on:change="setSound" :id="value.uuid">
+          <option value="">---</option>
+          <template v-for="(file, fileindex) in soundFiles" :key="fileindex">
+            <option :value="file.uuid" :selected="file.uuid === value.soundId">{{file.name}}</option>
+          </template>
+        </select>
+        <button :id="value.uuid" v-on:click="deleteCommand">X</button>
+      </div>
+    </div>
+    <hr>
     <div v-for="message in messages" :key=message>
       <span><b>{{ message.name }} :</b> {{ message.text }}</span>
     </div>
-    <hr>
-    <form v-on:submit.prevent="onSubmit">
-      <input type="file" v-on:change="fileChanged" id="file"/> <label for="file">file</label>
-    </form>
   </div>
 </template>
 
 <script>
-import { ref, toRef, computed, watch } from 'vue';
+import { ref, toRef, computed, watch, onBeforeUpdate } from 'vue';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
+import * as v4 from 'uuid/v4';
 
 export default {
   props: {
@@ -50,6 +73,16 @@ export default {
     const saveToStorage = ref(false);
     const russian = ref(false);
     const talking = ref(false);
+    const soundCommands = ref(JSON.parse(window.localStorage.getItem('soundCommands')) || []);
+    const soundFiles = ref([]);
+    const audios = ref([]);
+    const audioOnCommands = ref([]);
+    const command = ref("");
+
+    onBeforeUpdate (() => {
+      audios.value = [];
+      audioOnCommands.value = [];
+    }) 
 
     if (props.channel) {
       const channel = toRef(props, 'channel');
@@ -59,7 +92,7 @@ export default {
 
     function save(user, voicesettings) {
       if (saveToStorage.value) {
-        window.localStorage.setItem(user, JSON.stringify(voicesettings));
+        window.localStorage.setItem('user:' + user, JSON.stringify(voicesettings));
       } else {
         settings[user] = voicesettings;
       }
@@ -67,7 +100,7 @@ export default {
 
     function load(user) {
       if (saveToStorage.value) {
-        const item = window.localStorage.getItem(user);
+        const item = window.localStorage.getItem('user:' + user);
         if (item) {
           return JSON.parse(item);
         } else {
@@ -79,7 +112,18 @@ export default {
     }
     
     watch(store.state.messages, (msgs) => {
-      if (!talking.value) {
+      const lastMessage = msgs[msgs.length - 1];
+      const name = lastMessage.name;
+      const text = lastMessage.text;
+      let talk = talking.value;
+      
+      const index = soundCommands.value.findIndex(c => text.startsWith(c.name));
+      if (index > -1) {
+        talk = false;
+        playSound(soundCommands.value[index].soundId);
+      }
+
+      if (!talk) {
         return;
       }
       const synth = window.speechSynthesis;
@@ -87,9 +131,6 @@ export default {
       if (voices.length == 0) {
         return;
       }
-      const lastMessage = msgs[msgs.length - 1];
-      const name = lastMessage.name;
-      const text = lastMessage.text;
       console.log(name);
       console.log(text);
       if (name !== '###') {
@@ -145,14 +186,104 @@ export default {
         tx.onerror = () => reject(new Error('file storage error'));
 
         const store = tx.objectStore('sounds');
-        console.log(files[0]);
-        store.put(files[0], files[0].name);
+        const file = files[0];
+
+        store.put({file: file, uuid: v4()}, file.name);
+        const index = soundFiles.value.findIndex(f => f.name === file.name);
+        if (index > -1) {
+          soundFiles.value.splice(index, 1);
+        }
+        soundFiles.value.push({
+          name: file.name,
+          url: URL.createObjectURL(file),
+          uuid: file.uuid
+        });
       });
       conn.close();
     }
 
+    async function loadSounds() {
+      const conn = await openDb('talking');
+      const items = await new Promise((resolve, reject) => {
+        const result = conn.transaction(['sounds'], "readonly");
+        result.onerror = reject;
+        const getall = result.objectStore('sounds').getAll();
+        getall.onsuccess = (event) => resolve(event.target.result);
+        getall.onerror = reject;
+      });
+      // console.log(tx);
+      const URL = window.URL;
+      for (const i in items) {
+        const item = items[i];
+        soundFiles.value.push({
+          name: item.file.name,
+          url: URL.createObjectURL(item.file),
+          uuid: item.uuid
+        });
+      }
+      conn.close();
+    }
+
+    function addCommand() {
+      const uuid = v4();
+      soundCommands.value.push({uuid: uuid, name: command.value});
+      command.value = "";
+      window.localStorage.setItem('soundCommands', JSON.stringify(soundCommands.value));
+    }
+
+    function deleteCommand(e) {
+      const id = e.target.id;
+      const index = soundCommands.value.findIndex((item) => item.uuid === id);
+
+      if (index > -1) {
+        soundCommands.value.splice(index, 1);
+        window.localStorage.setItem('soundCommands', JSON.stringify(soundCommands.value));
+      }
+    }
+
+    async function deleteFile(name) {
+      const conn = await openDb('talking');
+      await new Promise((resolve, reject) => {
+        const tx = conn.transaction('sounds', 'readwrite');
+        tx.oncomplete = resolve;
+        tx.onerror = () => reject(new Error('file storage error'));
+
+        const store = tx.objectStore('sounds');
+        store.delete(name);
+      });
+      conn.close();
+    }
+
+    async function deleteSound(e) {
+      const id = e.target.id;
+      const index = soundFiles.value.findIndex((item) => item.uuid === id);
+
+      if (index > -1) {
+        await deleteFile(soundFiles.value[index].name);
+        soundFiles.value.splice(index, 1);
+      }
+    }
+
+    function setSound(e) {
+      const commandId = e.target.id;
+      const soundId = e.target.value;
+      const index = soundCommands.value.findIndex((item) => item.uuid === commandId);
+      if (index > -1) {
+        soundCommands.value[index].soundId = soundId;
+        window.localStorage.setItem('soundCommands', JSON.stringify(soundCommands.value));
+      }
+    }
+
+    function playSound(e) {
+      const soundId = typeof e === 'string' ? e : e.target.id;
+      audios.value[soundId].play();
+    }
+
+    loadSounds();
+
     return {
-      name, connect, messages, saveToStorage, russian, fileChanged, talking
+      name, connect, messages, saveToStorage, russian, fileChanged, talking, soundCommands, soundFiles, audios, command, 
+      addCommand, deleteCommand, setSound, deleteSound, playSound
     }
   },
 
