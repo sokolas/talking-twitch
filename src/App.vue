f3b68971fd6b67d0030edcae0fe146f9154ef050
Sokolas <sunrise.beast@gmail.com>
2021-08-04 10:32:43 +0200

    cosmetic

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <router-view/>
-  <div class="footer">
+  <div class="info">
     Entirely client-side TTS and sound commands tool for Twitch chat.
     Enable talking (done by your browser, tested in Firefox and Chrome) to listen to your chat in funny voices.
-    Add your sounds and assign commands to activte them.
+    Add your sounds and assign commands to activate them.
     If the commands don't trigger the sounds, make sure that you enabled the autoplay feature in your browser.
     <u>Remember, all the data is stored in your browser only so if you use another browser or another pc you'll have to configure everything from scratch.</u>
     <br>
@@ -16,14 +16,13 @@
     background-color:  #e6e2d3;
   }
 
-  .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    border-top: 1px solid;
+  .info {
+    border: 1px solid;
+    border-color: #b9936c;
+    border-radius: 3px;
     text-align: center;
     padding: 10px;
+    margin: 50px;
   }
 
   .center {
